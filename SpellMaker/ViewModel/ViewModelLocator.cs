1b018108de42a6c8fa60d90712c58ed9873f6b8c
Alec Parks <alexander.parks@here.com>
2016-09-22 12:06:46 -0500

    Add ViewModelLocator
    Added ViewModelLocator to set up view model returning

---
+++ SpellMaker/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Assisticant;
+
+namespace SpellMaker
+{
+	public class ViewModelLocator: ViewModelLocatorBase
+	{
+		private readonly Spell _spell;
+		
+		public ViewModelLocator()
+		{
+		}
+
+		public Object SpellViewModel
+		{
+			get { return ViewModel(() => new SpellViewModel(_spell));}
+		}
+	}
+}
